0abf35bf4e9a43b4175fc81d7d2c944c0ebe9e36
RiaMalhotraHestabit <RiaM.hestabit@gmail.com>
2026-01-20 17:26:52 +0530

    change style of sidebar links

--- week3-next-tailwind-frontend/components/ui/Sidebar.jsx
+++ week3-next-tailwind-frontend/components/ui/Sidebar.jsx
@@ -2,13 +2,13 @@ export default function Sidebar() {
   return (
     <aside className="w-64 min-h-screen bg-gray-500 shadow-md p-4">
       <nav className="space-y-4">
-        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium">
+        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium text-white hover:text-black">
           Home
         </p>
-        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium">
+        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium text-white hover:text-black">
           Analytics
         </p>
-        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium">
+        <p className="p-2 rounded cursor-pointer hover:bg-gray-200 font-medium text-white hover:text-black">
           Settings
         </p>
 
